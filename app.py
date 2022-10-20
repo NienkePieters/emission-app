5361c2be35c585c13de41411458bf903c76de415
Nienke Pieters <nienkepieters@live.nl>
2022-10-20 15:51:49 +0200

    created file for creating data

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from viktor.core import ViktorController
 from viktor.parametrization import ViktorParametrization, OptionField
-from viktor.views import SVGView
+from viktor.views import SVGView, SVGResult
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -8,7 +8,7 @@ import numpy as np
 from io import StringIO
 
 class Parametrization(ViktorParametrization):
-    modeltype = OptionField('Choose a model type for forecastnig co2 emissions:', options=['Naive', 'Exponential Smoothing', 'Linear Regression'], default='naive')
+    modeltype = OptionField('Choose a model type for forecastnig co2 emissions:', options=['Naive', 'Exponential Smoothing', 'Linear Regression'], default='Naive')
 
 
 class Controller(ViktorController):
@@ -21,23 +21,17 @@ class Controller(ViktorController):
         #initialize figure
         fig = plt.figure(figsize = (8,5))
 
+        from data import series
+
         if params.modeltype == 'Naive' :
             model_name = 'Naive Seasonal'
-            x = np.random.randn(10)
+            from data import forecast_naive
+            forecast = forecast_naive  
 
-        if params.modeltype == 'Exponentional Smoothing':
-            model_name = 'Exponentional Smoothing'
-            x = np.random.randn(10)
-
-        if params.modeltype == 'Linear Regression':
-            model_name = 'Linear Regression'
-            x = np.random.randn(10)
-        
-        if params.modeltype == None:
-            model_name = 'Linear Regression'
-            x = np.random.randn(10)      
-
-        plt.plot(x)
+        idx = -144
+        series[idx:].plot(label='Actual Values')
+        forecast[idx:].plot(label= 'Forecast')
+        plt.plot(series, forecast_naive)
         plt.title(model_name)
 
         svg_data = StringIO()
---
+++ data.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from darts import TimeSeries
+from darts.models import *
+from darts.metrics import *
+from darts.dataprocessing.transformers import Scaler
+import logging
+
+logging.disable(logging.CRITICAL)
+
+### ---- Read data ----
+
+df = pd.read_csv('emission-app/data/monthly_in_situ_co2_mlo.csv',
+                  comment = '"', header = [0,1,2], na_values = '-99.99')
+
+cols = [' '.join(col).replace(' ', '') for col in df.columns.values]
+df.set_axis(cols, axis = 1, inplace = True)
+
+# Converting Excel date format to datetime and setting as dataframe index
+df['datetime'] = pd.to_datetime(df['DateExcel'], origin = '1899-12-30', unit = 'D')
+df.set_index('datetime', inplace = True)
+df = df[['CO2filled[ppm]']]
+df.rename(columns = {'CO2filled[ppm]': 'CO2'}, inplace = True)
+df.dropna(inplace = True)
+df = df.resample('M').sum()
+
+#Loading the pandas dataframe to a TimeSeries object as required by the Darts library
+series = TimeSeries.from_dataframe(df)
+start = pd.Timestamp('123115')
+
+### Create a naive forecasting model
+model_naive = NaiveSeasonal(K = 12)
+forecast_naive = model_naive.historical_forecasts(series, start=start, forecast_horizon=12, verbose=True)
+
+def naive_model(series):
+    model_naive = NaiveSeasonal(K=12)
+    forecast_naive = model_naive.historical_forecasts(series, start=start, forecast_horizon=12, verbose=True)
+    return forecast_naive
